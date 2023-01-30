d745570f91bb6ff4c68aa18ed31c94ee1aeb5461
manyids2 <arun.mukundan@gmail.com>
2023-01-30 19:39:36 +0100

    Paginate

--- README.md
+++ README.md
@@ -12,17 +12,18 @@ Uses [dearpygui](https://github.com/hoffstadt/DearPyGui).
 
 Dataframes with images
 
-[x] Show rows of images from urls.
-[x] Show/hide images.
-[x] Reload csv file.
-[ ] Paginate and dynamic textures.
-[ ] Load column data types from config.
-[ ] Layout rows with images and rest from config.
-[ ] Show/hide columns.
-[ ] Sort columns ( needs to be global ).
-[ ] Filter columns ( needs to be global ).
-[ ] Meta column with multiple images and switch.
-[ ] Comments and listboxes for rows.
+- [x] Show rows of images from urls.
+- [x] Show/hide images.
+- [x] Reload csv file.
+- [x] Paginate.
+- [ ] Dynamic textures.
+- [ ] Load column data types from config.
+- [ ] Layout rows with images and rest from config.
+- [ ] Show/hide columns.
+- [ ] Sort columns ( needs to be global ).
+- [ ] Filter columns ( needs to be global ).
+- [ ] Meta column with multiple images and switch.
+- [ ] Comments and listboxes for rows.
 
 ```
 ┌─┬─────────────────────────────────────────────────────┬─┐
--- pyguis/urlimages.py
+++ pyguis/urlimages.py
@@ -14,7 +14,13 @@ from pyguis.utils.images import image_from_url
 class Settings:
     height: int = 1000
     width: int = 1000
+
     show_images = True
+    n_rows: int = 0
+
+    current: int = 1
+    n_pages: int = 1
+    n_per_page: int = 2
 
 
 class App:
@@ -38,9 +44,20 @@ class App:
         image_columns = args.image_columns.split("+")
         return cls(args.csv_path, image_columns)
 
+    def set_page(self):
+        s = self.settings
+        start = (s.current - 1) * s.n_per_page
+        end = min(s.current * s.n_per_page, len(self.df))
+        self.rows = self.df.iloc[start:end, :]
+
     def load_csv(self):
         self.df = pd.DataFrame(pd.read_csv(self.csv_path))
         self.columns = list(self.df.columns)
+        s = self.settings
+        s.n_rows = len(self.df)
+        s.current = 1
+        s.n_pages = int(np.ceil(len(self.df) / s.n_per_page))
+        self.set_page()
 
     def setup_window(self):
         width, height = self.settings.width, self.settings.height
@@ -74,6 +91,36 @@ class App:
         del sender
         user_data.csv_path = Path(app_data)
 
+    @staticmethod
+    def handle_first(sender, app_data, user_data):
+        del sender, app_data
+        user_data.settings.current = 1
+        user_data.set_page()
+        user_data.render()
+
+    @staticmethod
+    def handle_prev(sender, app_data, user_data):
+        del sender, app_data
+        user_data.settings.current = max(1, user_data.settings.current - 1)
+        user_data.set_page()
+        user_data.render()
+
+    @staticmethod
+    def handle_next(sender, app_data, user_data):
+        del sender, app_data
+        user_data.settings.current = min(
+            user_data.settings.n_pages, user_data.settings.current + 1
+        )
+        user_data.set_page()
+        user_data.render()
+
+    @staticmethod
+    def handle_last(sender, app_data, user_data):
+        del sender, app_data
+        user_data.settings.current = user_data.settings.n_pages
+        user_data.set_page()
+        user_data.render()
+
     def setup_handlers(self):
         with dpg.handler_registry():
             dpg.add_key_press_handler(callback=self.key_press_handler)
@@ -92,7 +139,7 @@ class App:
 
     def render_settings(self):
         with dpg.group(label="settings", parent="primary_window"):
-            with dpg.table():
+            with dpg.table(header_row=False):
                 dpg.add_table_column()  # Show/hide
                 dpg.add_table_column()  # csv_file
                 dpg.add_table_column()  # reload csv
@@ -112,20 +159,60 @@ class App:
                     )
                     dpg.add_button(label="Reload csv", callback=self.reload)
 
+    def render_pages(self):
+        with dpg.group(label="pages", parent="primary_window", tag="pages_group"):
+            with dpg.table(header_row=False):
+                dpg.add_table_column()  # First
+                dpg.add_table_column()  # Prev
+                dpg.add_table_column()  # Current
+                dpg.add_table_column()  # Total
+                dpg.add_table_column()  # Next
+                dpg.add_table_column()  # Last
+                with dpg.table_row():
+                    dpg.add_button(
+                        label="First",
+                        user_data=self,
+                        callback=self.handle_first,
+                    )
+                    dpg.add_button(
+                        label="Prev",
+                        user_data=self,
+                        callback=self.handle_prev,
+                    )
+                    dpg.add_text(
+                        f"{self.settings.current}",
+                        # callback=self.handle_current,
+                    )
+                    dpg.add_text(
+                        f" / {self.settings.n_pages}",
+                    )
+                    dpg.add_button(
+                        label="Next",
+                        user_data=self,
+                        callback=self.handle_next,
+                    )
+                    dpg.add_button(
+                        label="Last",
+                        user_data=self,
+                        callback=self.handle_last,
+                    )
+
     def reload(self):
         self.load_csv()
         self.render()
 
     def render(self):
+        dpg.delete_item("pages_group")
         dpg.delete_item("table_group")
+        self.render_pages()
         with dpg.group(tag="table_group", parent="primary_window"):
             with dpg.table(header_row=True, tag="main_table"):
                 for column in self.columns:
                     dpg.add_table_column(label=column)
 
-                for i in range(len(self.df)):
+                for i in range(len(self.rows)):
                     with dpg.table_row(tag=f"table__row-{i}"):
-                        row = self.df.iloc[i, :]
+                        row = self.rows.iloc[i, :]
                         for column in self.columns:
                             tag = f"table__cell-{i}-{column}"
                             is_image = column in self.image_columns
